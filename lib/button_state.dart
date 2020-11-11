df06b462cdeaabef3c6460c9ad14ffe6dd8363c2
ihassanjavaid <great.maliks@gmail.com>
2020-11-11 19:29:41 +0500

    init

---
+++ lib/button_state.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ButtonState extends ChangeNotifier {
+  Color _buttonOneColor = Colors.white;
+  Color _buttonTwoColor = Colors.white;
+  Color _buttonThreeColor = Colors.white;
+  String _textString = 'Selected Button';
+
+  Color get buttonOneColor => _buttonOneColor;
+  Color get buttonTwoColor => _buttonTwoColor;
+  Color get buttonThreeColor => _buttonThreeColor;
+  String get textString => _textString;
+
+  void selectButton(int button) {
+    switch (button) {
+      case 1:
+        _buttonOneColor = Colors.black;
+        _buttonTwoColor = Colors.white;
+        _buttonThreeColor = Colors.white;
+        _textString = 'One';
+        break;
+
+      case 2:
+        _buttonOneColor = Colors.white;
+        _buttonTwoColor = Colors.black;
+        _buttonThreeColor = Colors.white;
+        _textString = 'Two';
+        break;
+
+      case 3:
+        _buttonOneColor = Colors.white;
+        _buttonTwoColor = Colors.white;
+        _buttonThreeColor = Colors.black;
+        _textString = 'Three';
+        break;
+      default:
+        print('Wrong.');
+        break;
+    }
+
+    notifyListeners();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'button_state.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => ButtonState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Button Provider',
+        theme: ThemeData.dark(),
+        home: MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Button States'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Selected Button: ${Provider.of<ButtonState>(context).textString}',
+              style: TextStyle(fontSize: 40, color: Colors.white),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Provider.of<ButtonState>(context, listen: false)
+                    .selectButton(1);
+              },
+              color: Provider.of<ButtonState>(context).buttonOneColor,
+              child: Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Raised Button One',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Provider.of<ButtonState>(context, listen: false)
+                    .selectButton(2);
+              },
+              color: Provider.of<ButtonState>(context).buttonTwoColor,
+              child: Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Raised Button Two',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            RaisedButton(
+              onPressed: () {
+                Provider.of<ButtonState>(context, listen: false)
+                    .selectButton(3);
+              },
+              color: Provider.of<ButtonState>(context).buttonThreeColor,
+              child: Container(
+                width: 180,
+                height: 50,
+                child: Center(
+                  child: Text(
+                    'Raised Button Three',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
